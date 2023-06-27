2beaaa64e4dab0df15f670db845463212c3ad5c0
iruojefaith <faithiruoje@gmail.com>
2023-06-27 20:31:39 +0100

    added Dashbaord page

--- src/Pages/Analytics.tsx
+++ src/Pages/Analytics.tsx
@@ -25,20 +25,20 @@ const Analytics = () => {
             </div>
             <br />
             <h4>Starting out as a Product designer</h4>
-            <p className="tex-sm flex gap-2 items-center"><span> <VscBook/> </span> 10 mins read</p>
+            <p className="tex-sm flex gap-2 items-center text-[#626262]"><span className='text-[#000]'> <VscBook/> </span> 10 mins read</p>
             <br />
-            <p>Embarking on a journey as a product designer can be an exhilarating and fulfilling experience. As a profession that bridges the realms of art, technology, and problem-solving, product design offers an opportunity to shape the way people interact with the world around them.</p>
+            <p className='text-[#626262] -mt-[2rem]'>Embarking on a journey as a product designer can be an exhilarating and fulfilling experience. As a profession that bridges the realms of art, technology, and problem-solving, product design offers an opportunity to shape the way people interact with the world around them.</p>
             <div className="flex justify-between">
                 <div>
-                    <div className='flex gap-2 items-center'><span><AiOutlineComment/> </span> 200 </div>
+                    <div className='flex gap-2 items-center text-[#626262]'><span><AiOutlineComment/> </span> 200 </div>
                 </div>
                 <div className="h-3"></div>
                 <div>
-                    <div className='flex gap-2 items-center'><span><AiOutlineHeart/> </span> 120 </div>
+                    <div className='flex gap-2 items-center text-[#626262]'><span><AiOutlineHeart/> </span> 120 </div>
                 </div>
                 <div className="h-3"></div>
                 <div>
-                    <div className='flex gap-2 items-center'><span><MdOutlineAnalytics/> </span> 2190 views </div>
+                    <div className='flex gap-2 items-center text-[#626262]'><span><MdOutlineAnalytics/> </span> 2190 views </div>
                 </div>
             </div>
             <br/>
@@ -54,24 +54,24 @@ const Analytics = () => {
         <div className="w-full md:w-1/2 ">
             <div className="flex justify-between">
                 <div className="text-center">
-                  <p className="font-bold">Posts</p>
+                  <p className="font-bold text-[#626262]">Posts</p>
                   <h5>3</h5>
                 </div>
                 <div className="text-center">
-                  <p className="font-bold">Posts Impressions</p>
+                  <p className="font-bold text-[#626262]">Posts Impressions</p>
                   <h5>2.98k Views</h5>
                 </div>
             </div>
             <br />
             <div className="flex justify-between">
                 <div className="text-center">
-                  <p className="font-bold">Profile visits</p>
-                  <h5>300</h5>
+                  <p className="font-bold text-[#626262]">Profile visits</p>
+                  <h5 className='text-[#000]'>300</h5>
                 </div>
 
                 <div className="text-center">
-                  <p className="font-bold">New Followers</p>
-                  <h5>300</h5>
+                  <p className="font-bold text-[#626262]">New Followers</p>
+                  <h5 className='text-[#000]'>300</h5>
                 </div>
             </div>
         </div>
