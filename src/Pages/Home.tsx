645120aef8404c23c881b8fc28cf4ad40e64905c
iruojefaith <faithiruoje@gmail.com>
2023-06-28 21:33:27 +0100

    fix error message

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Nav from '../components/Navbar/Navbar'
 import Header from '../components/Home/Header'
 import About from '../components/Home/About'
 import Join from '../components/Home/Join'
 import ChatterSection from '../components/Home/ChatterSection'
 import Footer from '../components/Footer/Footer'
-import Nav from '../components/Navbar/Navbar'
+
 
 
 const Home = () => {
